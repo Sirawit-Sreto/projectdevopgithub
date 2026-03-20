c92fd3abd48dc2171a640454fb2138fa1e589655
sirawit_Sreto <sirawit.sre@ku.th>
2026-03-20 05:59:25 +0000

    Update logo image and modify footer text in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="80ku.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
         <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
+          By SirawitSreto
         </p>
         <p>
           <a
